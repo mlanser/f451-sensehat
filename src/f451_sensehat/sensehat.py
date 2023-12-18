3329c1ccdb141d550e6d60cb7ead9f51bac27a76
Martin Lanser <martinlanser@gmail.com>
2023-12-18 17:04:18 -0500

    tweak sparkles

--- src/f451_sensehat/sensehat.py
+++ src/f451_sensehat/sensehat.py
@@ -541,20 +541,20 @@ class SenseHat:
         # Do we want to clear the screen? Or add more sparkles?
         maxSparkle = int(DISPL_MAX_COL * yMax * MAX_SPARKLE_PCNT)
         if randint(0, maxSparkle):
-            # x, y, rgb = _sparkle()
-            numSparkles = randint(0, maxSparkle)
-            print(numSparkles)
+            x, y, rgb = _sparkle()
+            # numSparkles = randint(0, maxSparkle)
+            # print(numSparkles)
             # pixels = [(0,0,0) for _ in range(DISPL_MAX_COL * yMax)]
-            for _ in range(numSparkles):
-                x, y, rgb = _sparkle()
-                self._SENSE.set_pixel(x, y, rgb)
+            # for _ in range(numSparkles):
+            #     x, y, rgb = _sparkle()
+            #     self._SENSE.set_pixel(x, y, rgb)
                 # indx = y * yMax + x
                 # pixels[indx] = rgb
 
             # self._SENSE.set_pixels(pixels)
-            # self._SENSE.set_pixel(x, y, rgb)
+            self._SENSE.set_pixel(x, y, rgb)
         else:
-            print("-CLEAR-")
+            # print("-CLEAR-")
             self._SENSE.clear()
 
     def display_8x8_image(self, image):
