97783a32faa96a820054aa0cdaa6d0cdfb0868f9
Martin Lanser <martinlanser@gmail.com>
2023-12-29 15:31:23 -0500

    bump version 1.4.6 -> 1.4.7

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# f451 Labs SenseHat module v1.4.6
+# f451 Labs SenseHat module v1.4.7
 
 ## Overview
 
--- src/f451_sensehat/__init__.py
+++ src/f451_sensehat/__init__.py
@@ -1,4 +1,4 @@
 """f451 Labs SenseHat module."""
 
-__version__ = '1.4.6'
+__version__ = '1.4.7'
 __app_name__ = 'f451-sensehat'
